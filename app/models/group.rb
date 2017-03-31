c678ecc001c0e2af246bee51bd6a6bcdcd9a7030
young <yangzhanfeng@yangzhanfengdeMacBook-Air.local>
2017-03-31 22:32:56 +0800

    add user_id to group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :user
   validates :file_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331134026) do
+ActiveRecord::Schema.define(version: 20170331142905) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "file_name"
     t.text     "file_description"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
